99da09d4e0edde6e5c798708275b606e7874c1fd
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-12-30 23:26:37 +0000

    Fix puppeteer deployment issue

--- src/scrapper.js
+++ src/scrapper.js
@@ -3,7 +3,14 @@ const util = require('./util');
 
 exports.scrape = async function (address, options) {
 
-    const browser = await puppeteer.launch({ headless: true, defaultViewport: { width: 1024, height: 780, isMobile: false } });
+    const browser = await puppeteer.launch({
+        headless: true,
+        defaultViewport: { width: 1024, height: 780, isMobile: false },
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
+    });
     const page = await browser.newPage();
 
     try {
